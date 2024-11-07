03edd7338f5db1c7a1f5f7cfb22aae526d830114
JONG GEUN <0young384@gmail.com>
2024-11-07 16:43:20 +0900

    feat(#9): 밥메이트, 간식내기 게시글 단건 조회 api

--- routes/board.js
+++ routes/board.js
@@ -26,4 +26,20 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Todo /api/board -> /api/main 으로 변경
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const board = await Board.findById(id);
+
+    if (!board) {
+      return res.status(404).json({ message: "글을 찾을 수 없습니다." });
+    }
+
+    res.status(200).json(board);
+  } catch (error) {
+    res.status(500).json({ message: "글 조회 실패", error });
+  }
+});
+
 module.exports = router;
--- routes/gameBoard.js
+++ routes/gameBoard.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const GameBoard = require("../models/GameBoard");
 
+// 게임 게시글 추가 API
 router.post("/", async (req, res) => {
   try {
     const { name, title, content, winner, isEnd, totalCount, currentCount, participate } = req.body;
@@ -18,9 +19,25 @@ router.post("/", async (req, res) => {
     });
 
     const savedGamePost = await newGamePost.save();
-    res.status(201).json(savedGamePost); // 저장된 전체 객체를 응답으로 반환
+    res.status(201).json(savedGamePost); 
   } catch (error) {
     res.status(500).json({ message: "게시글 저장 실패", error });
+  }
+});
+
+// Todo /api/board -> /api/main 으로 변경
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const gameBoard = await GameBoard.findById(id);
+
+    if (!gameBoard) {
+      return res.status(404).json({ message: "게임 게시글을 찾을 수 없습니다." });
+    }
+
+    res.status(200).json(gameBoard);
+  } catch (error) {
+    res.status(500).json({ message: "게임 게시글 조회 실패", error });
   }
 });
 
