1515aa4a6c67c8cf2991cf187a3817622da545a8
JONG GEUN <0young384@gmail.com>
2024-11-08 08:32:26 +0900

    Setting(#12): MongoDB 한국시간으로 설정

--- models/GameBoard.js
+++ models/GameBoard.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 
 const gameBoardSchema = new mongoose.Schema({
-  // game_board_id: {
-  //   type: Number,
-  //   required: true,
-  //   unique: true,
-  // },
   name: {
     type: String,
     required: true,
@@ -18,20 +14,25 @@ const gameBoardSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
   winner: {
     type: String,
-    required: true,
+    default: null,
   },
+<<<<<<< Updated upstream
   participate: {
     type: [String], // 참여자 리스트
+=======
+  createdAt: {
+    type: String,
+    default: () => moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
+>>>>>>> Stashed changes
   },
   isEnd: {
     type: Boolean,
     required: true,
+  },
+  participate: {
+    type: [String], // 참여자 리스트
   },
   totalCount: {
     type: Number,
@@ -43,5 +44,11 @@ const gameBoardSchema = new mongoose.Schema({
   },
 });
 
-const GameBoard = mongoose.model("gameBoard", gameBoardSchema);
+// 새 문서가 저장될 때 자동으로 createdAt을 한국 시간으로 포맷하여 저장
+gameBoardSchema.pre("save", function (next) {
+  this.createdAt = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
+  next();
+});
+
+const GameBoard = mongoose.model("gameboard", gameBoardSchema);
 module.exports = GameBoard;
