cfc0563d9a0c6417eec0ce5ceac48c8c42d503d2
ZZZINU <nnjin987@gmail.com>
2024-11-07 16:43:49 +0900

    Feat(#7): 게시글 목록 조회 API 구현

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Board = require("../models/Board");
+const Board = require('../models/Board');
 
 // 게시글 추가 API
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const { name, title, content, category, isAnonymous, isEnd, participate, totalCount, currentCount } = req.body;
+    const {
+      name,
+      title,
+      content,
+      category,
+      isAnonymous,
+      isEnd,
+      participate,
+      totalCount,
+      currentCount,
+    } = req.body;
 
     const newPost = new Board({
       name,
@@ -22,7 +32,7 @@ router.post("/", async (req, res) => {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
   } catch (error) {
-    res.status(500).json({ message: "게시글 저장 실패", error });
+    res.status(500).json({ message: '게시글 저장 실패', error });
   }
 });
 
