7616037301af8f08f5645698e608ee97b53d5b03
goeunP <0young384@gmail.com>
2024-11-07 20:52:04 +0900

    Fix(#2): fix merge conflict

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var userRouter = require("./routes/user");
+var boardRouter = require("./routes/board");
+var gameBoardRouter = require("./routes/gameBoard");
+var mainRouter = require("./routes/main");
 var app = express();
 const mongoose = require("./db");
 const cors = require("cors");
@@ -23,25 +26,22 @@ app.use(
   })
 );
 
-
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/", indexRouter);
 app.use("/api", userRouter);
-app.use('/api/board', boardRouter);
-app.use('/api/gameBoard', gameBoardRouter);
-app.use('/api/main', mainRouter);
-
+app.use("/api/board", boardRouter);
+app.use("/api/gameBoard", gameBoardRouter);
+app.use("/api/main", mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -52,11 +52,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json('error');
+  res.json("error");
 });
 
 module.exports = app;
