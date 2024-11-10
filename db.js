5a36ad62a0f00ccec2095a1b80b5ebd809a53378
JONG GEUN <6813821@tukorea.ac.kr>
2024-11-11 01:19:07 +0900

    Setting(#27): 테스트 DB 추가

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const MONGO_HOST = `mongodb+srv://dobapmin:${process.env.DB_PASSWORD}@dobapmin.gypgs.mongodb.net/?retryWrites=true&w=majority&appName=Dobapmin`;
+// const MONGO_HOST = `mongodb+srv://testDobapmin:${process.env.TEST_DB_PASSWORD}@testdobapmin.f70hi.mongodb.net/?retryWrites=true&w=majority&appName=testDobapmin`;
 mongoose
   .connect(MONGO_HOST, {
     retryWrites: true,
